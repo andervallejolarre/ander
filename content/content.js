1d5b20b85290c4df8de792387d88ce8cb9fdb3fe
andervallejolarre <130057768+andervallejolarre@users.noreply.github.com>
2025-02-19 20:48:12 +0100

    Update content.js

--- content/content.js
+++ content/content.js
@@ -61,8 +61,8 @@ const projects = [
       { image: '11_Pozos.JPG' },
       //{ image: '12_Pozos.JPG' },
       { image: '13_Pozos.jpg' },
+      { image: '15_Pozos.JPG' },
       { image: '14_Pozos.jpg' },
-      //{ image: '15_Pozos.JPG' },
       { image: '16_Pozos.JPG' }
     ],
   },
