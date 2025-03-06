c8007734ce450fdf9df2094da7c6a436b87ab065
andervallejolarre <130057768+andervallejolarre@users.noreply.github.com>
2025-03-06 12:29:13 +0100

    Update about.js

--- script/about.js
+++ script/about.js
@@ -15,6 +15,7 @@ const textSection = `
 
 const imageSection = `
   <div class="about-image-section">
+    <img src="../content/images/${aboutContent.image}">,
     <img src="../content/images/${aboutContent.image}">
   </div>
 `
